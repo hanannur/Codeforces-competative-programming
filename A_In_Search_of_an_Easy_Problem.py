f49b625b0b8e6dd25202b51751418a3acd063242
Hanan-Nuru <hanannuru65@gmail.com>
2025-12-06 22:53:07 +0300

    edit in search of an easy problem

--- A_In_Search_of_an_Easy_Problem.py
+++ A_In_Search_of_an_Easy_Problem.py
@@ -1,8 +1,15 @@
+# n=int(input())
+# i=list(map(int,input().split()))
+# for j in range(n):
+#     if i[j]==1:
+#         print("HARD")
+#         break
+# else:
+#     print("EASY")
 n=int(input())
-i=list(map(int,input().split()))
-for j in range(n):
-    if i[j]==1:
-        print("HARD")
-        break
+s=list(map(int , input().split()))
+set_s=set(s)
+if 1 in s:
+    print("HARD")
 else:
     print("EASY")
